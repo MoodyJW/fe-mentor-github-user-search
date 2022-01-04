aba3456fe3b5e748b0ebd41ffd36e04872b69e5c
MoodyJW <jmoody1813@gmail.com>
2022-01-04 13:08:11 -0600

    created base components, structured main html, removed ng stuff, added FE mentor icon files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ThemeToggleComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
