bae3d7e2e984b7bd0412f96091f5ec1160921479
MoodyJW <jmoody1813@gmail.com>
2022-01-04 16:31:04 -0600

    toggle works, but colors on svg are annoying, might need to just do it inline or something instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'github-user-search-ng';
+  theme = 'dark-theme';
+
+  toggleTheme(currentTheme: string) {
+    this.theme = currentTheme;
+  }
 }
