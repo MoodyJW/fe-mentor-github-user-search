e3dcfc520ae36fe60b6d78ec6ef893c0f4e83976
MoodyJW <jmoody1813@gmail.com>
2022-01-18 16:04:24 -0600

    seems done on mobile, removed material and just used css because fuck material thats why

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
@@ -18,13 +15,7 @@ import { CardComponent } from './components/card/card.component';
     ThemeToggleComponent,
     CardComponent,
   ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    MatIconModule,
-    MatInputModule,
-    NoopAnimationsModule,
-  ],
+  imports: [AppRoutingModule, BrowserModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
