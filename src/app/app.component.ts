d3ae707cdd57316156d63a543b9eb195f035a9b0
MoodyJW <jmoody1813@gmail.com>
2022-06-13 11:19:54 -0500

    updated readme, cleaned up some code, fixed styling errors, added theme detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
 import { User } from './models/user.model';
-import { DEFAULT_USER } from './constants/default-user.constant';
+import { UsersService } from './services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  theme = 'dark-theme';
-  user: User = DEFAULT_USER;
+  theme = window.matchMedia('(prefers-color-scheme: light)').matches
+    ? 'light-theme'
+    : 'dark-theme';
+  user!: User;
+
+  constructor(private usersService: UsersService) {
+    this.usersService
+      .getUser('octocat')
+      .pipe(first())
+      .subscribe((user) => {
+        this.user = user;
+      });
+  }
 
   toggleTheme(currentTheme: string) {
     this.theme = currentTheme;
